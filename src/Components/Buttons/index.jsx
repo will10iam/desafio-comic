18bdfba43c66262fd3a88a14e6f2448db7274e98
William Berbet <wiiliamb@hotmail.com>
2023-03-21 22:41:17 -0300

    configs, componentes e primeira página

---
+++ src/Components/Buttons/index.jsx
@@ -0,0 +1,29 @@
+import styled from 'styled-components';
+
+export default function Button(props) {
+    return (
+        <>
+            <Bbutton>
+                {props.name}
+            </Bbutton>
+
+        </>
+    )
+}
+
+
+// ==================================== STYLES ===============================//
+
+const Bbutton = styled.div`
+    background-color: #F26722;
+    border-radius: 8px;
+    margin-top: 150px;
+    width: 120px;
+    text-align: center;
+    padding: 10px;
+    color: #fff;
+    font-family: 'League Spartan', sans-serif;
+    font-weight: 700;
+    font-size: 20px;
+    margin-left: 10px;
+`
---
+++ src/Components/Header/index.jsx
@@ -0,0 +1,38 @@
+import styled from 'styled-components';
+import { Link } from 'react-router-dom';
+import marvel from '../../Assets/46.svg'
+import cart from '../../Assets/47.svg'
+
+export default function Header() {
+    return (
+        <>
+            <HHeader>
+                <Link to="/"><img src={marvel} alt="" className='logo' /></Link>
+                <Link to="/"><img src={cart} alt="" className='cart' /></Link>
+            </HHeader>
+        </>
+    )
+}
+
+//--------------------------------- STYLES -----------------------------------------//
+
+const HHeader = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    width: 100%;
+    height: 100px;
+    background-color: #000000;
+
+    & .logo {
+        width: 50%;
+        margin-left: 5em;
+        
+    }
+
+    & .cart {
+        width: 30%;
+        margin-left: 5em;
+        
+    }
+`
